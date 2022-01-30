73983461aee149c6c12fef4e524636781cc1569b
Azamat <you@example.com>
2022-01-31 00:53:03 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// let financial__middle = document.querySelector('.financial__middle')
+
+// const arr = [1,2,3]
+
+// function show(arr) {
+
+//     financial__middle.innerHTML=arr.map(el => `<div>${el}</div>` );
+
+// }
+// show(arr)
+// let worker = ['Петров', 'Иванов', 'Васильев', 'Яковлева', 'Степанова'];
+// let out_arr = document.getElementById('out_arr');
+// let str = ' ';
+// for (let i = 0; i< worker.length; i++  ) {
+// if (worker[i]!==undefined) str +=i+' - '+worker[i]+'<br>';
+// }
+// out_arr.innerHTML = str;
+
